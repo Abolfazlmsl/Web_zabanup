38ec1a41383039f6e7f373778c507194ccd85f1f
Amirhossein <amirhossein_barari76@yahoo.com>
2019-07-17 13:05:03 +0430

    create submit view and calculate grade

--- Reading/views.py
+++ Reading/views.py
@@ -15,16 +15,19 @@ def home(request):
 
 
 def reading(request):
-    passages = models.Passage.objects.all()
+    if request.user.is_authenticated:
+        passages = models.Passage.objects.all()
 
-    query_g1 = request.GET.get("group1")
-    query_g2 = request.GET.get("group2")
-    if query_g1 and query_g2:
-        # pass
-        return render(request, 'Reading/Detail.html', {'group1': query_g1, 'group2': query_g2})
+        query_g1 = request.GET.get("group1")
+        query_g2 = request.GET.get("group2")
+        if query_g1 and query_g2:
+            # pass
+            return render(request, 'Reading/Detail.html', {'group1': query_g1, 'group2': query_g2})
 
-    context = {'passages': passages}
-    return render(request, 'Reading/reading.html', context)
+        context = {'passages': passages}
+        return render(request, 'Reading/reading.html', context)
+    else:
+        return redirect('Reading:home')
 
 
 def passage_body(request, passage_id):
@@ -33,18 +36,64 @@ def passage_body(request, passage_id):
 
 
 def submit(request, passage_id):
-    # if request.method == 'POST':
-    passage = get_object_or_404(models.Passage, pk=passage_id)
     if request.method == 'POST':
-        answer_list = request.POST.getlist('answer')
-        # answer_list = passage.question_set.get(pk=request.POST.getlist('question.id'))
-    context = {
-        'List': answer_list,
-        'passage': passage,
-    }
-    return render(request, 'Reading/submit.html', context)
+        answer_list = []
+        answer_text = []
+        passage = get_object_or_404(models.Passage, pk=passage_id)
 
+        q1 = request.POST.get('q1')
+        answer_list.append(q1)
+        q2 = request.POST.get('q2')
+        answer_list.append(q2)
+        q3 = request.POST.get('q3')
+        answer_list.append(q3)
+        q7 = request.POST.get('q7')
+        answer_list.append(q7)
+        q8 = request.POST.get('q8')
+        answer_list.append(q8)
+        q9 = request.POST.get('q9')
+        answer_list.append(q9)
 
+        hidden1 = request.POST.get('hidden1')
+        hidden2 = request.POST.get('hidden2')
+        hidden3 = request.POST.get('hidden3')
+        q4 = request.POST.get('q4')
+        q5 = request.POST.get('q5')
+        q6 = request.POST.get('q6')
+        answer_text.append([hidden1, q4])
+        answer_text.append([hidden2, q5])
+        answer_text.append([hidden3, q6])
+
+        q10 = request.POST.getlist('q10')
+        q10_id = request.POST.get('q10_id')
+
+        grade = 0
+        for answer in answer_list:
+            my_answer = models.Answer.objects.get(id=answer).truth
+            if my_answer:
+                grade += 1
+
+        for answer in answer_text:
+            my_question = models.Question.objects.get(id=answer[0])
+            if answer[1] == models.Answer.objects.get(question=my_question).text:
+                grade += 1
+
+        count_q10 = 0
+        answers_10 = models.Answer.objects.filter(question=q10_id)
+        for answer in answers_10:
+            if answer.truth:
+                count_q10 += 1
+        for answer in q10:
+            if models.Answer.objects.get(id=answer).truth:
+                count_q10 -= 1
+        if count_q10 == 0:
+            grade += 1
+        context = {
+            'List': answer_list,
+            'passage': passage,
+            'grade': grade*10,
+        }
+        return render(request, 'Reading/submit.html', context)
 
 
 def login_view(request):
