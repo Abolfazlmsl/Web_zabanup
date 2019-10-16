6a95040c7e45bda125fc3c84679827046bfba61c
amirhossein <mr.amirhossein1836@gmail.com>
2019-10-16 10:41:22 +0330

    test

--- Reading/api/urls.py
+++ Reading/api/urls.py
@@ -2,4 +2,5 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('exam/', views.ExamList.as_view(), name='ExamList'),
+    path('exam/<int:pk>', views.PassageList.as_view(), name='PassageList'),
 ]
--- Reading/api/views.py
+++ Reading/api/views.py
@@ -1,9 +1,34 @@
+from django.db.models import Q
+from django.shortcuts import get_list_or_404
 from rest_framework import generics
 from Reading import models
 from . import serializers
 
 
 class ExamList(generics.ListAPIView):
-
-    queryset = models.Exam.objects.all()
     serializer_class = serializers.ExamSerializers
+
+    def get_queryset(self):
+        query = models.Exam.objects.all()
+
+        book = self.request.GET.get('book')
+        category = self.request.GET.get('category')
+        difficulty = self.request.GET.get('difficulty')
+
+        if book is not None:
+            book_list = book.split(",")
+            query = query.filter(
+                Q(book__in=book_list)
+            ).distinct()
+        if category is not None:
+            category_list = category.split(",")
+            query = query.filter(
+                Q(category__in=category_list)
+            ).distinct()
+        if difficulty is not None:
+            difficulty_list = difficulty.split(',')
+            query = query.filter(
+                Q(difficulty__in=difficulty_list)
+            ).distinct()
+        return query
+
