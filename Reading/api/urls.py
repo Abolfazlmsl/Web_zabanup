4eda2a52515ed289350a8d3ca065afbaf796dc89
amirhossein_bri <amirhossein_barari76@yahoo.com>
2019-10-16 10:38:02 +0330

    api

--- Reading/api/urls.py
+++ Reading/api/urls.py
@@ -2,4 +2,9 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('exam/', views.ExamList.as_view(), name='ExamList'),
+    path('passage/', views.PassageList.as_view(), name='PassageList'),
+    path('question/', views.QuestionList.as_view(), name='QuestionList'),
+    path('answer/', views.AnswerList.as_view(), name='AnswerList'),
+    path('user/', views.UserList.as_view(), name='UserList'),
+    path('user/<int:pk>/', views.UserDetail.as_view(), name='UserDetail'),
 ]
