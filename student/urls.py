d4814977af264d52aeb3f40212da16a86bd4927a
dariush abedi <abedi.1989@gmail.com>
2019-12-26 07:43:08 +0000

    Update urls.py

--- student/urls.py
+++ student/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.conf.urls import url
 from . import views
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls.static import static 
 
 # Urls of app
 
