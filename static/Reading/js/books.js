9c82d031c6e9b32fa963c7688a50209aa5f3b22b
Amir Tavakoli <Amirtavakoli76@gmail.com>
2020-01-05 18:06:49 +0330

    Filter

--- static/Reading/js/books.js
+++ static/Reading/js/books.js
@@ -36,33 +36,7 @@
 // }
 //
 // // Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
-$('.owl-carousel').owlCarousel({
-    rtl:true,
-    loop:true,
-    margin:10,
-    nav:true,
-    // autoplay:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:4
-        }
-    }
-});
+
 
 function filterSelection(category){
     if(category === 'all'){
@@ -118,3 +92,186 @@ $("#toggle-cambridge-books").click(function () {
         $("#toggle-cambridge-books").removeClass('rotate-down-sidebar');
     }
 });
+
+getData()
+function getData() {
+    $.ajax({
+        url: 'http://127.0.0.1:8000/api/book/',
+        type: "GET",
+        dataType: "json",
+        data: {
+
+        },
+        success: function (data) {
+            // console.log(JSON.stringify(data));
+            let books = data["book"];
+            let categories = data["category"];
+            let questionType = data["question_type"];
+            let exam = data["exams"];
+
+            //Categories information
+            let categoryInfo = [];
+            categories.map(item => {
+                categoryInfo.push({
+                    id: item["id"],
+                    name: item["name"]
+                })
+            });
+
+            categoryInfo.map(item => {
+                $("#exam-categories").append(
+                    `<button class="btn btn-sm btn-rounded" style="font-size: 12px" id="category-${item.id}" onclick="filterSelection('${item.name}')">${item.name}</button>`
+                )
+            });
+            console.log(categoryInfo)
+            activeBtn();
+
+            // Books information
+            let bookInfo = [];
+
+            books.map(item => {
+                bookInfo.push({
+                    id: item["id"],
+                    name: item["name"],
+                    image: item["image"]
+                })
+            });
+            bookInfo.map(item => {
+                $("#cambridge-book-choices").append(
+                    `<div class="form-check d-flex align-items-center my-2 p-0" style="direction: ltr">` +
+                        `<input class="form-check-input" type="checkbox" id="book-${item.id}">` +
+                        `<label class="form-check-label"  for="book-${item.id}" style="font-size: 16px; font-family: segoe">` +
+                            `${item.name}` +
+                        `</label>` +
+                    `</div>`
+                )
+            });
+
+
+            // Questions Type information
+            let questionTypeInfo = [];
+
+            questionType.map(item => {
+                questionTypeInfo.push({
+                    name: item["name"],
+                    value: item["value"]
+                });
+            });
+            console.log(questionTypeInfo.length);
+            questionTypeInfo.map(item => {
+                $("#question-type-choices").append(
+                    `<div class="form-check d-flex align-items-center my-2 p-0" style="direction: ltr">` +
+                        `<input class="form-check-input" type="checkbox" id="${item.name}">` +
+                        `<label class="form-check-label"  for="${item.name}" style="font-size: 17px">` +
+                            `${item.value}` +
+                        `</label>` +
+                    `</div>`
+                )
+            })
+
+            // Exam information
+            let examInfo = [];
+            exam.map(item => {
+                examInfo.push({
+                    id: item["id"],
+                    book: item["book"],
+                    name: item["name"],
+                    image: item["image"],
+                    categories: item["categories"],
+                    question_type: item["questions_type"]
+                })
+            });
+            // console.log(examInfo);
+            // let examsOfCategory = [];
+            let examList = [];
+            for (let i = 0; i < categoryInfo.length; i++) {
+                let exam = [];
+                for (let j = 0; j < examInfo.length; j++) {
+                    for (let k = 0; k < examInfo[j].categories.length; k++) {
+                        if (categoryInfo[i].name === examInfo[j].categories[k].name) {
+                            exam.push(examInfo[j]);
+                        }
+                    }
+                }
+                examList.push({
+                    category: categoryInfo[i].name,
+                    exams: exam
+                })
+            }
+            console.log(examList);
+            examList.map(item => {
+                $("#show-exams-container").append(
+                    `<div class="col-12 ${item.category}-box">` +
+                        `<b class="mr-2" style="font-size: 18px">${item.category}</b>` +
+                        `<hr class="mt-1 mb-4">` +
+                        `<div class="book-carousel" id="${item.category}">` +
+
+                        `</div>` +
+                        `<div class="row filtered-${item.category} d-none mx-0">` +
+                            `<div class="${item.category}-filtered book-cart item mx-auto mx-md-2 my-1">` +
+
+                            `</div>` +
+                        `</div>` +
+                    `</div>`
+                );
+                item.exams.map(itm => {
+                    console.log(item.category)
+                    $(`#${item.category}`).append(
+                        `<div class="book-cart mx-auto item zoom">` +
+                            `<div class="content">` +
+                                `<img src="${itm.image}" alt="Mountains" style="width:100%">` +
+                                `<h6 class="mt-3">${itm.name}</h6>` +
+                                `<h6 class="mt-3">${itm.categories.name}</h6>` +
+                                `<p>Description of ${itm.book}</p>` +
+                            `</div>` +
+                        `</div>`
+                    )
+                })
+            })
+
+//       `<div class="content">` +
+//                                     `<img src="{% static 'Reading/Images/zabanup.jpg' %}" alt="Mountains" style="width:100%">` +
+//                                     `<h4 class="mt-3">Book 1</h4>` +
+//                                     `<p>Description of Book 1</p>` +
+//                                 `</div>` +      //
+            // console.log(examInfo)
+            addOwl()
+        }
+
+    })
+}
+
+function activeBtn() {
+    var btnContainer = document.getElementById("exam-categories");
+    var btns = btnContainer.getElementsByClassName("btn");
+    for (var i = 0; i < btns.length; i++) {
+        console.log("btns: " + btns.length);
+      btns[i].addEventListener("click", function(){
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+      });
+    }
+}
+
+function addOwl() {
+    $(".book-carousel").addClass('owl-carousel').addClass('owl-theme');
+    $('.owl-carousel').owlCarousel({
+        rtl:true,
+        loop:false,
+        margin:10,
+        nav:true,
+        // autoplay:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    });
+}
