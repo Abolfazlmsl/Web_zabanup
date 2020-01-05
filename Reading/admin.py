30a519d4d75ec0bca670c0f256349e8fd50d16bf
Amir Tavakoli <Amirtavakoli76@gmail.com>
2020-01-05 15:11:02 +0330

    css

--- Reading/admin.py
+++ Reading/admin.py
@@ -6,7 +6,6 @@ from . import models
 admin.site.register(models.Profile)
 admin.site.register(models.Answer)
 admin.site.register(models.UserAnswer)
-admin.site.register(models.ExamCategory)
 admin.site.register(models.Exam)
 admin.site.register(models.ExamCategory)
 admin.site.register(models.Comment)
